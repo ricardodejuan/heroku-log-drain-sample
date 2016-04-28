e7a89ba6c7763b5e708bb10dec5c794cf414771c
J. Ricardo de Juan Cajide <jricardo.dejuan@gmail.com>
2016-04-28 11:03:39 +0200

    little change

--- process_logs.go
+++ process_logs.go
@@ -28,7 +28,7 @@ func (r *routerLog) HandleLogfmt(key, val []byte) error {
 func processLogs(w http.ResponseWriter, r *http.Request) {
 	c := redisPool.Get()
 	defer c.Close()
-	fmt.Printf("REQUEST ", r)
+	fmt.Printf("REQUEST ", r.Body)
 	lp := lpx.NewReader(bufio.NewReader(r.Body))
 	// a single request may contain multiple log lines. Loop over each of them
 	for lp.Next() {
